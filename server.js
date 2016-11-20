9b5c484c83fbfa7c3481b9d5eb58cd19bbd6b6d7
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-19 23:33:46 -0800

    add routes GET, POST, USE and create and do exercise PUT and DELETE methods.

--- server.js
+++ server.js
@@ -32,5 +32,38 @@ if (require.main === module) {
     });
 };
 
+var Item = require('./models/item');
+
+app.get('/items', function(req, res) {
+    Item.find(function(err, items) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.json(items);
+    });
+});
+
+app.post('/items', function(req, res) {
+    Item.create({
+        name: req.body.name
+    }, function(err, item) {
+        if (err) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            });
+        }
+        res.status(201).json(item);
+    });
+});
+
+app.use('*', function(req, res) {
+    res.status(404).json({
+        message: 'Not Found'
+    });
+});
+
+
 exports.app = app;
 exports.runServer = runServer;
