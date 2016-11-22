330c7ade2e3f034236cfc50e8a34c898a4a8f7c6
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-22 11:50:31 -0800

    fixed the code for POST had two GET method routes.

--- test/test-server.js
+++ test/test-server.js
@@ -45,26 +45,23 @@ describe('Shopping List', function() {
     });
 });
     //Applying and Copying the POST test for the shopping list from the shopping list API.
-        it('should list items on GET', function(done) {
-            chai.request(app)
-            .get('/items')
-            .end(function(err, res) {
-                should.equal(err, null);
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('array');
-                res.body.should.have.length(3);
-                res.body[0].should.be.a('object');
-                res.body[0].should.have.property('_id');
-                res.body[0].should.have.property('name');
-                res.body[0]._id.should.be.a('string');
-                res.body[0].name.should.be.a('string');
-                res.body[0].name.should.equal('Broad beans');
-                res.body[1].name.should.equal('Tomatoes');
-                res.body[2].name.should.equal('Peppers');
+    it('should add an item on POST', function(done) {
+        chai.request(app)
+        .post('/items')
+        .send({'name': 'Kale'})
+        .end(function(err, res) {
+            should.equal(err, null);
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('name');
+            res.body.should.have.property('_id');
+            res.body.name.should.be.a('string');
+            res.body._id.should.be.a('string');
+            res.body.name.should.equal('Kale');
         done();
-      });
-  });
+    });
+});
 //Applying and Copying the PUT test for the shopping list from the shopping list API.      
         it('should edit an item on PUT', function(done) {
             chai.request(app)
