2622b05038cfa283931f78fcfef0df003f94e95f
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-21 22:47:25 -0800

    Added and commented for before and after function and also added POST

--- test/test-server.js
+++ test/test-server.js
@@ -12,6 +12,7 @@ var app = server.app;
 chai.use(chaiHttp);
 
 describe('Shopping List', function() {
+    //Given the before function is run before each test is run. In this function you run the server, then you seed the database by adding some sample data which you can use in any of your tests.
     before(function(done) {
         server.runServer(function() {
             Item.create({name: 'Broad beans'},
@@ -21,28 +22,54 @@ describe('Shopping List', function() {
             });
         });
     });
-  it('should list items on GET', function(done) {
-    chai.request(app)
-      .get('/items')
-      .end(function(err, res) {
-        should.equal(err, null);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('array');
-        res.body.should.have.length(3);
-        res.body[0].should.be.a('object');
-        res.body[0].should.have.property('_id');
-        res.body[0].should.have.property('name');
-        res.body[0]._id.should.be.a('string');
-        res.body[0].name.should.be.a('string');
-        res.body[0].name.should.equal('Broad beans');
-        res.body[1].name.should.equal('Tomatoes');
-        res.body[2].name.should.equal('Peppers');
+  //Applying and Copying the GET test for the shopping list from the shopping list API.  
+    it('should list items on GET', function(done) {
+        chai.request(app)
+        .get('/items')
+        .end(function(err, res) {
+            should.equal(err, null);
+            res.should.have.status(200);
+            res.should.be.json;
+            res.body.should.be.a('array');
+            res.body.should.have.length(3);
+            res.body[0].should.be.a('object');
+            res.body[0].should.have.property('_id');
+            res.body[0].should.have.property('name');
+            res.body[0]._id.should.be.a('string');
+            res.body[0].name.should.be.a('string');
+            res.body[0].name.should.equal('Broad beans');
+            res.body[1].name.should.equal('Tomatoes');
+            res.body[2].name.should.equal('Peppers');
+        done();
+    });
+});
+    //Applying and Copying the POST test for the shopping list from the shopping list API.
+      it('should add an item on POST', function(done) {
+        chai.request(app)
+        .post('/items')
+        .send({'name': 'Kale'})
+        .end(function(err, res) {
+            should.equal(err, null);
+            res.should.have.status(201);
+            res.should.be.json;
+            res.body.should.be.a('object');
+            res.body.should.have.property('name');
+            res.body.should.have.property('id');
+            res.body.name.should.be.a('string');
+            res.body.id.should.be.a('number');
+            res.body.name.should.equal('Kale');
+            storage.items.should.be.a('array');
+            storage.items.should.have.length(4);
+            storage.items[3].should.be.a('object');
+            storage.items[3].should.have.property('id');
+            storage.items[3].should.have.property('name');
+            storage.items[3].id.should.be.a('number');
+            storage.items[3].name.should.be.a('string');
+            storage.items[3].name.should.equal('Kale');
         done();
       });
-
-  });
-
+  });  
+    //Given the after function, which is run after each test is run, you remove all of the items from the database so you are starting from a clean slate for the next test
     after(function(done) {
         Item.remove(function() {
             done();
