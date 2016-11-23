a2585b19b30b7868cb90b864df7d53d814a85d73
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-22 20:16:28 -0800

    apply push

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+exports.DATABASE_URL = 'mongodb://test:test@ds053166.mlab.com:53166/testshoppinglist' 
+// process.env.DATABASE_URL || 
+//                        global.DATABASE_URL || 'mongodb://test:test@ds053166.mlab.com:53166/testshoppinglist' ||
+//                        (process.env.NODE_ENV === 'production' ?
+//                             'mongodb://localhost/shopping-list' :
+//                             'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
+
