698ea4c6fc35efd485676887d5bec6498da6582e
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-23 19:51:34 -0800

    changed package.json

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// //exports.DATABASE_URL = 'mongodb://test:test@ds053166.mlab.com:53166/testshoppinglist' 
+//exports.DATABASE_URL = 'mongodb://test:test@ds053166.mlab.com:53166/testshoppinglist' 
 // process.env.DATABASE_URL || 
 //                        global.DATABASE_URL || 'mongodb://test:test@ds053166.mlab.com:53166/testshoppinglist' ||
 //                        (process.env.NODE_ENV === 'production' ?
